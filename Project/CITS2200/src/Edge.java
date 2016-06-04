6490cc933dbc72d587a04e26ad1d6d0cea10cb26
Yiyang <gyy150@163.com>
2016-06-05 05:02:19 +0800

    betweeness centrality nearly done

---
+++ Project/CITS2200/src/Edge.java
@@ -0,0 +1,14 @@
+
+public class Edge {
+	int Vertex1Index;
+	int Vertex2Index;
+	int Vertex1;
+	int Vertex2;
+	
+	public Edge(int Vertex1Index, int Vertex2Index,  int Vertex1,  int Vertex2 ){
+		this.Vertex1Index = Vertex1Index;
+		this.Vertex2Index = Vertex2Index;
+		this.Vertex1 = Vertex1;
+		this.Vertex2 = Vertex2;
+	}
+}
